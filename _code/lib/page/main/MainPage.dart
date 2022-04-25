4976350036365dc5543c312d7de2a221a2b84300
TryIt <imkim1893@naver.com>
2022-04-25 17:06:29 +0900

    MainPage 추가

---
+++ _code/lib/page/main/MainPage.dart
@@ -0,0 +1,41 @@
+
+import 'package:flutter/material.dart';
+import 'package:insta_crawller_flutter/_common/abstract/KDHState.dart';
+import 'package:insta_crawller_flutter/_common/util/PageUtil.dart';
+import 'package:insta_crawller_flutter/util/MyFonts.dart';
+import 'package:insta_crawller_flutter/util/MyTheme.dart';
+
+class MainPage extends StatefulWidget {
+  static const String staticClassName = "MainPage";
+  final className = staticClassName;
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends KDHState<MainPage> {
+  @override
+  Future<void> onLoad() async {}
+
+  @override
+  Future<void> mustRebuild() async {
+    toBuild = () => Scaffold(
+            body: Container(
+          width: 350,
+          color: MyTheme.mainColor,
+          alignment: Alignment.center,
+          child: Text(
+            "Insta\nManager",
+            textAlign: TextAlign.center,
+            style: MyFonts.coiny(
+              fontSize: 35,
+              height: 1.1,
+              color: MyTheme.subColor,
+            ),
+          ),
+        ));
+
+    rebuild();
+  }
+}

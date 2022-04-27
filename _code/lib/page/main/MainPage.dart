3042cf05667344f159c4dc7bcf43657d14aaccb2
donghyun kim <imkim1893@naver.com>
2022-04-27 17:49:07 +0900

    MainPage.dart 틀 잡기 완료

--- _code/lib/page/main/MainPage.dart
+++ _code/lib/page/main/MainPage.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:insta_crawller_flutter/_common/abstract/KDHState.dart';
-import 'package:insta_crawller_flutter/util/MyFonts.dart';
 import 'package:insta_crawller_flutter/util/MyTheme.dart';
 
 class MainPage extends StatefulWidget {
   static const String staticClassName = "MainPage";
   final className = staticClassName;
-  
+
   const MainPage({Key? key}) : super(key: key);
 
   @override
@@ -19,22 +18,65 @@ class _MainPageState extends KDHState<MainPage> {
 
   @override
   Future<void> mustRebuild() async {
-    toBuild = () => Scaffold(
-            body: Container(
-          width: 350,
+    toBuild = () {
+      return Scaffold(
+        body: Container(
           color: MyTheme.mainColor,
           alignment: Alignment.center,
-          child: Text(
-            "MainPage",
-            textAlign: TextAlign.center,
-            style: MyFonts.coiny(
-              fontSize: 35,
-              height: 1.1,
-              color: MyTheme.subColor,
-            ),
+          child: Column(
+            children: [
+              appBar(),
+              Divider(),
+              Expanded(child: scrollView()),
+            ],
           ),
-        ));
-
+        ),
+      );
+    };
     rebuild();
+  }
+
+  Widget appBar() {
+    return Row(
+      children: [
+        Text("appBar"),
+      ],
+    );
+  }
+
+  Widget scrollView() {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          instaCardGroup(),
+        ],
+      ),
+    );
+  }
+
+  Widget instaCardGroup() {
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Icon(Icons.error),
+                  Text("abc__"),
+                  Icon(Icons.error),
+                ],
+              ),
+              Container(
+                height: 215,
+                color: Colors.blue,
+              ),
+            ],
+          ),
+        ),
+        Divider(),
+      ],
+    );
   }
 }

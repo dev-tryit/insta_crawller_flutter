e843ee464c3b89ff2095cb54f7402370bad10761
donghyun kim <imkim1893@naver.com>
2022-04-28 22:57:35 +0900

    이모지 추가 완료

--- _code/lib/page/main/MainPage.dart
+++ _code/lib/page/main/MainPage.dart
@@ -1,6 +1,9 @@
+import 'package:emojis/emoji.dart';
 import 'package:flutter/material.dart';
 import 'package:insta_crawller_flutter/_common/abstract/KDHState.dart';
+import 'package:insta_crawller_flutter/_common/extension/RandomExtension.dart';
 import 'package:insta_crawller_flutter/util/MyFonts.dart';
+import 'package:insta_crawller_flutter/util/MyImage.dart';
 import 'package:insta_crawller_flutter/util/MyTheme.dart';
 
 class MainPage extends StatefulWidget {
@@ -38,7 +41,7 @@ class _MainPageState extends KDHState<MainPage> {
 
   Widget appBar() {
     return Padding(
-      padding: const EdgeInsets.only(left:18, top:18),
+      padding: const EdgeInsets.only(left: 18, top: 18),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -55,7 +58,17 @@ class _MainPageState extends KDHState<MainPage> {
                 ),
               ),
               Spacer(),
-              Image(width:16,height:20,image: AssetImage('images/notes_icon.png'), fit: BoxFit.contain),
+              InkWell(
+                child: Image(
+                  width: 16,
+                  height: 20,
+                  image: MyImage.notesIcon,
+                  fit: BoxFit.contain,
+                ),
+                onTap: () {
+                  print("InkWell onTap");
+                },
+              ),
               SizedBox(width: 26),
             ],
           ),
@@ -67,26 +80,44 @@ class _MainPageState extends KDHState<MainPage> {
 
   Widget scrollView() {
     return SingleChildScrollView(
-      child: Column(
-        children: List.generate(12, (index) => instaCardGroup()),
+      child: Padding(
+        padding: EdgeInsets.only(top:34),
+        child: Column(
+          children: List.generate(12, (index) => instaCardGroup()),
+        ),
       ),
     );
   }
 
   Widget instaCardGroup() {
+    String emojiChar = Emoji.byGroup(EmojiGroup.smileysEmotion).toList().getRandomElement().char;
+
     return Column(
       children: [
         Container(
-          padding: const EdgeInsets.only(left:18, right: 24),
+          padding: const EdgeInsets.only(left: 18, right: 24),
           child: Column(
             children: [
               Row(
                 children: [
-                  Icon(Icons.error),
+                  Text(emojiChar),
+                  SizedBox(width: 5),
                   Text("abc__"),
-                  Icon(Icons.error),
+                  SizedBox(width: 5),
+                  InkWell(
+                    child: Image(
+                      width: 18,
+                      height: 18,
+                      image: MyImage.downloadsIcon,
+                      fit: BoxFit.fill,
+                    ),
+                    onTap: (){
+                      print("downloadsIcon click");
+                    },
+                  )
                 ],
               ),
+              SizedBox(height: 8),
               Container(
                 height: 215,
                 color: Colors.blue,
--- _code/lib/util/MyImage.dart
+++ _code/lib/util/MyImage.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
 
 class MyImage {
-  static const AssetImage backgroundTop =
-      AssetImage('images/backgroundTop.png');
-  static const AssetImage boxIcon = AssetImage('images/boxIcon.png');
-  static const AssetImage plusIcon = AssetImage('images/plusIcon.png');
-  static const AssetImage minusIcon = AssetImage('images/minusIcon.png');
+  static const AssetImage notesIcon = AssetImage('images/notes_icon.png');
+  static const AssetImage downloadsIcon = AssetImage('images/downloads_icon.png');
 }
